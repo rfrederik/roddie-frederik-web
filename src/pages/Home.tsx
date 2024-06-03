5ab31ca3f86365250f68d2d61582e469a48ee13b
rfrederik <roddie.frederik@gmail.com>
2024-06-03 18:09:39 -0500

    resize image on big screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import ugotcigs from '../assets/ugotcigs.webp';
 const Home: React.FC = () => {
   return (
     <div className="p-4">
-        <img src={ugotcigs} alt="Ugotcigs" className="mx-auto" />
+        <img src={ugotcigs} alt="Ugotcigs" className="mx-auto md:w-1/4 w-full" />
     </div>
   );
 }
